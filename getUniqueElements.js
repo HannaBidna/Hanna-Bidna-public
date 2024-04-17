4ab150eb4612d31fb16164548756207e9503722e
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2024-04-17 22:26:24 +0200

    partial reverse

--- getUniqueElements.js
+++ getUniqueElements.js
@@ -1,3 +1,4 @@
+// Створити функцію getUniqueElements яка отримує масив (примітивів) і повертає масив без дублікатів. Наприклад getUniqueElements([2, 7, 7, 5, 4, 2, 7]) має повернути [2, 7, 5, 4]. Використання Set заборонено.
 function getUniqueElements(arr){
     arr.sort ((a, b) => a-b);
     let result = [];
