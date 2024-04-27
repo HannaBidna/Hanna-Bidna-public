6a67b3c09a06b38b4e8cd08093012a78d1592fa5
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2024-04-27 22:18:00 +0200

    reduce

--- reverse-reduce.js
+++ reverse-reduce.js
@@ -1,1 +1,8 @@
-//Напишіть функцію яка міняє порядок літер в рядку на зворотній. Для вирішення обовʼязково використати reduce.
+//Напишіть функцію, яка міняє порядок літер в рядку на зворотній. Для вирішення обовʼязково використати reduce.
+function revertOrder(str){
+    let arr = str.split('');
+    
+    let revertString = arr.reduce((reverted, item) => item + reverted);
+    return revertString;
+   }
+   console.log (revertOrder('Hello'));//olleH
