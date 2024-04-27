b59e37e54d6e0d06bfff82534a7d146ff57fd698
HannaBidna <130675463+HannaBidna@users.noreply.github.com>
2024-04-27 23:10:26 +0200

    less one

--- lessone.js
+++ lessone.js
@@ -1,1 +1,9 @@
 //Написати функцію яка повертає true якщо в масиві є 2 сусідніх елементи (числа) які відрізняються на одиницю. В іншому випадку повертається false. Для вирішення обовʼязково використати методи масивів.
+
+function lessOne(arr){
+    return arr.some((element, index, array) => {
+        return Math.abs(element - array[index + 1]) === 1;
+    });
+    }
+  console.log(lessOne([1,3,5,9,8]));// true
+  console.log(lessOne([3,3,7,5,9,0]));// false
